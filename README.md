04ae7020fe38e9a22d03c11c80ba991d71ec2b2a
Rohit Kumar <rokumar@pivotal.io>
2015-09-23 13:05:40 -0600

    Create example-nozzle
    Signed-off-by: Ergin Babani <ebabani@pivotal.io>

---
+++ README.md
@@ -0,0 +1,3 @@
+=Summary=
+
+The example nozzle is part of the [example-nozzle-release](https://github.com/cloudfoundry-incubator/example-nozzle-release) BOSH release. 
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"os"
+
+	"github.com/cloudfoundry/noaa"
+	"github.com/cloudfoundry/sonde-go/events"
+	"flag"
+	"log"
+	"github.com/cloudfoundry-incubator/uaago"
+)
+
+func main() {
+
+	user := flag.String("user", "example-nozzle", "user who has access to the firehose")
+	password := flag.String("pass", "example-nozzle", "password for the user")
+	trafficControllerURL := flag.String("tcurl", "wss://doppler.bosh-lite.com:443", "loggregator traffic controller URL and port")
+	uaaURL := flag.String("uaaurl", "https://uaa.bosh-lite.com", "UAA URL")
+
+	uaaClient, err := uaago.NewClient(*uaaURL)
+	if err != nil {
+		log.Fatalf("Error creating uaa client: %s", err.Error())
+	}
+
+	var authToken string
+	authToken, err = uaaClient.GetAuthToken(*user, *password, true)
+	if err != nil {
+		log.Fatalf("Error getting oauth token: %s. Please check your username and password.", err.Error())
+	}
+
+	connection := noaa.NewConsumer(*trafficControllerURL, &tls.Config{InsecureSkipVerify: true}, nil)
+
+	fmt.Println("===== Streaming Firehose (will only succeed if you have admin credentials)")
+
+	msgChan := make(chan *events.Envelope)
+	go func() {
+		defer close(msgChan)
+		errorChan := make(chan error)
+		const firehoseSubscriptionId = "firehose-a"
+		go connection.Firehose(firehoseSubscriptionId, authToken, msgChan, errorChan)
+
+		for err := range errorChan {
+			fmt.Fprintf(os.Stderr, "%v\n", err.Error())
+		}
+	}()
+
+	for msg := range msgChan {
+		fmt.Printf("%v \n", msg)
+	}
+}
